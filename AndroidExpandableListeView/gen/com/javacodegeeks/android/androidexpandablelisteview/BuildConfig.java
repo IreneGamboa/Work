fb089155640d6162a68e6e4f5cbd3780dcd7df59
IreneGamboa <ireniuxgp@gmail.com>
2014-05-06 12:33:01 -0600

    Ejemplo ListView Group

---
+++ AndroidExpandableListeView/gen/com/javacodegeeks/android/androidexpandablelisteview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.javacodegeeks.android.androidexpandablelisteview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
